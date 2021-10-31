78c2e63b43b4fb97de166a4520496d4eb77ef5f2
shivendrad <dshivendra88@gmail.com>
2021-11-01 01:16:32 +0530

    code organiserd

--- or.py
+++ or.py
@@ -4,13 +4,15 @@ import numpy as np
 import pandas as pd 
 
 
-def main(data, eta, epochs, filename, plotFilename):
+def main(data, eta, epochs, plotFilename, filename):
 
     df = pd.DataFrame(data)
     print(df)
 
     X,y = prepare_data(df)
+    ETA = 0.3
 
+    EPOCHS = 10
 
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X,y)
@@ -21,13 +23,12 @@ def main(data, eta, epochs, filename, plotFilename):
     save_plot(df, plotFilename, model)
 
 if __name__=='__main__': #'<<<entry point'
-    AND = {
+    OR = {
         "x1": [0,0,1,1],
         "x2": [0,1,0,1],
-        "y":  [0,0,0,1],
+        "y":  [0,1,1,1],
     }
-
     ETA = 0.3
     EPOCHS = 10
 
-    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and.model", plotFilename="and.png")
+    main(data=OR, eta=ETA, epochs=EPOCHS, filename="or.model", plotFilename="or.png")
